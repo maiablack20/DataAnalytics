6ce130f76cd1eaedfdf44332f9caaa5f170ef1d2
maiablack20 <blackmaia143@gmail.com>
2026-04-15 16:27:25 -0400

    Added queries of actor and film tables

--- week-02/Ex1B_first_query.sql
+++ week-02/Ex1B_first_query.sql
@@ -1,3 +1,9 @@
 /*
 The purpose of sakila to assist in helping students learn to navigate and work within a database as practice. 
 */
+USE sakila;
+SELECT * FROM actor;
+SHOW TABLES;
+-- Retrieved 200 records
+SELECT * FROM film;
+-- Retrieved 100 records
