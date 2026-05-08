62e43e8bcfd145afcd5d5662d33792c2078c7af2
maiablack20 <blackmaia143@gmail.com>
2026-05-08 00:54:59 -0400

    Added Exercise 2A docs

--- week-05/Ex2A_MathScripts/area_of_circle.py
+++ week-05/Ex2A_MathScripts/area_of_circle.py
@@ -6,3 +6,14 @@
 # 4. How do you calculate the area of a circle?
     
 # a) The diameter of a given circle is the same as the day of your birthday (not the month,just the day). Figure out the formula, refresh your recollection of the difference between diameter and radius, and figure out what the script should look like.
+
+import math
+
+diameter = 20 
+
+radius = int((diameter/2))
+
+area = (math.pi * radius**2)
+print(f"The area of a circle with a radius of {radius}  is: {area:.2f}")
+
+# The area of a circle with a radius of 10 is 314.16.
