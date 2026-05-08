f3b63300402ebd411f60d052c3c3664f36c410d7
maiablack20 <blackmaia143@gmail.com>
2026-05-07 23:55:59 -0400

    Added 2A exercise

---
+++ week-05/Ex2A_MathScripts/area_of_rectangle.py
@@ -0,0 +1,16 @@
+# Maia Black
+# 2026-05-07 
+
+# Calculating the area of a rectangle
+
+# 2. How do you calculate the area of a rectangle?
+    
+    # a) Say you have a rectangle that has dimensions corresponding to your birthday – the month number is one side and the day of the month is the other side. How would you calculate the area of this rectangle?
+
+length = int(input("Enter Side A in feet: "))
+width = int(input("Enter Side B in feet: "))
+
+area_of_rectangle = (length * width)
+print(" Area of a rectangle is: ", area_of_rectangle)
+
+# Using my birthday month, March (3) and the day (20) as the length and width of the rectangle, the area of the rectangle came to be 60 feet. 
