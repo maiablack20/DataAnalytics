616293441e2a2223a8c77b080fc762fda5d768d9
maiablack20 <blackmaia143@gmail.com>
2026-05-11 12:28:12 -0400

    Added bonus exercise

---
+++ week-05/Ex4A_IfScripts/bonus_gregorian_calendar.py
@@ -0,0 +1,56 @@
+# Maia Black 
+# 2026-05-11
+
+# Gregorian Calendar Bonus Assignment
+
+# What determines if a leap year is a leap year is if the year is divisible by 4, but if the year can be divided by 100 as well as four, then it's not a leap year. However, if the year is divisble by 400, then it is a leap year. 
+
+year = 1900
+
+if year % 4 == 0: 
+        print("This is a leap year")
+else: 
+        print("This is not a leap year")
+# This is a leap year.
+
+year = 1950 
+
+if year % 4 == 0: 
+        print("This is a leap year")
+else: 
+        print("This is not a leap year")
+# This is not a leap year. 
+
+year = 1999
+
+if year % 4 == 0: 
+        print("This is a leap year")
+else: 
+        print("This is not a leap year")
+# This is not a leap year. 
+
+year = 2000
+
+if year % 4 == 0: 
+        print("This is a leap year")
+else: 
+        print("This is not a leap year")
+# This is a leap year. 
+
+year = 2001 
+
+if year % 4 == 0: 
+        print("This is a leap year")
+else: 
+        print("This is not a leap year")
+# This is not a leap year. 
+
+year = 20212
+
+if year % 4 == 0: 
+        print("This is a leap year")
+else: 
+        print("This is not a leap year")
+# This is a leap year. 
+
+# The code has worked because this is the exact equation that I had performed extensive research about online and figured out that it was the correct code and equation to use for this. 
