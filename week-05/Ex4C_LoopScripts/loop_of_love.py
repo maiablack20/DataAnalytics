3d15ea4b781de99eb911410554ebfda0cfa66678
maiablack20 <blackmaia143@gmail.com>
2026-05-12 01:49:42 -0400

    Added Loops exercise docs

---
+++ week-05/Ex4C_LoopScripts/loop_of_love.py
@@ -0,0 +1,17 @@
+# Maia Black
+# 2026-05-12 
+
+# The original file name provided in the workbook was deemed as not valid for Python and the name had to be edited in order to become valid. 
+
+# Create a list of words that mean great such as amazing, awesome, execellent, etc. 
+
+# Use a for loop to iterate through your list, and with each loop print out the phrase "Loops are ___!" filling in the blank with a word from the list. 
+
+# When your loop has finished, print the additional phrase "I <3 loops."
+
+great_words = ["Miraculous", "Good", "Sweet", "Awesome", "Superb"]
+
+for word in great_words: 
+    print("Loops are", word +"!")
+
+print("I <3 loops!")
