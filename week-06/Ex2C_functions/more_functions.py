c6c8a7bb4f1520f29102d1a036515df61a703e57
maiablack20 <blackmaia143@gmail.com>
2026-05-17 20:03:07 -0400

    Updated More Functions Exercise for Bonus Question Answered

--- week-06/Ex2C_functions/more_functions.py
+++ week-06/Ex2C_functions/more_functions.py
@@ -4,15 +4,18 @@
 # Displaying Mailing Label Exercise 
 
 # First Function: 
-def display_mailing_label(name, address, city, state, zip):
+def display_mailing_label(name, address, city, state, zip, address2 = None):
     print(f"=====================================")
     print(f"{name}")
     print(f"{address}")
+    if address2 is not None: 
+            print(f"{address2}")
     print(f"{city}, {state} {zip}")
 
 display_mailing_label(f"Virgil Hawkins", "2234 Electric Way", "Dakota", "Illinois", "23445")
 display_mailing_label(f"Kiyomi Simmons", "2523 Walnut Crest Lane", "Houston", "Texas", "77001")
 display_mailing_label(f"Seraphina Williams", "2400 Sun Crest Way", "Atlanta", "Georgia", "30033")
+display_mailing_label(f"Brandy Morton", "2300 South Fannie Lou Hamer Boulevard", "Atlanta", "Georgia", "30301", address2 = "Apt.F246")
 
 # Second Function: 
 def add_numbers(*numbers):
@@ -33,21 +36,25 @@ add_numbers(1, 79, 350)
 
 
 # Third Function
-def display_receipt(total_due, amount_paid):
-    print(f"Total Due: ${total_due:.2f}")
+def display_receipt(*total_due, amount_paid):
+    totals = sum(total_due)
+    print(f"Totals: ${totals:.2f}")
     print(f"Amount Paid: ${amount_paid:.2f}")
-    if total_due > amount_paid: 
-        remaining_balance = total_due - amount_paid
+    if totals > amount_paid: 
+        remaining_balance = totals - amount_paid
         print(f"Remaining Balance: ${remaining_balance:.2f}")
         print(f"=====================================")
-    elif amount_paid == total_due:
+    elif amount_paid == totals:
         print(f"Transaction Approved")
         print(f"=====================================")
     else: 
-        change_due = amount_paid - total_due
+        change_due = amount_paid - totals
         print(f"Change Due: ${change_due:.2f}")
         print(f"=====================================")
 
-display_receipt(20.50, 109.50)
-display_receipt(13.50, 13.50)
-display_receipt(24.30, 25.00)
+display_receipt(20.50, amount_paid = 109.50)
+display_receipt(13.50, amount_paid = 13.50)
+display_receipt(24.30, amount_paid = 25.00)
+
+# BONUS: 
+display_receipt(40.00, 32.00, amount_paid = 3.00)
